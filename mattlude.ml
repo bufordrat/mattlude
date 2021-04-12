9f275e1e57bbfe43586d6f4e910f988c90a5c4a7
Matt Teichman <teichman@uchicago.edu>
2021-04-11 20:36:22 -0500

    applied NameOfModule.Make naming regimen

--- mattlude.ml
+++ mattlude.ml
@@ -101,8 +101,8 @@ module type ERROR = sig
 end
                 
 module Result = struct
-  (* module functor for building a Result module with cool extra stuff in it;
-   takes a module containing the error type as an input *) 
+  (* module functor for building a Result module with cool extra stuff
+     in it; takes a module containing the error type as an input *) 
   module Make (E : ERROR) = struct
     include Result
     
@@ -138,99 +138,101 @@ module type TOKEN = sig
   val cons : 'tok -> 'tok t -> 'tok t
 end
 
-(* IS short for 'input stream'
- * OS short for 'output stream' *)
-module ParserF (OS: TOKEN) (IS : TOKEN) = struct
-  module PResult = Result.Make (String)
 
-  module ParserMonad = struct
-    type 'output t =
-      IS.stream -> (('output * IS.stream), string) result
-    let pure x = fun stream -> PResult.ok (x, stream)
-    let bind prsr k = let open PResult in 
-                      fun input ->
-                      let* (result1, remainder1) = prsr input in
-                      (k result1) remainder1
+module Parser = struct
+  (* IS short for 'input stream'
+   * OS short for 'output stream' *)
+  module Make (OS: TOKEN) (IS : TOKEN) = struct
+    module PResult = Result.Make (String)
+
+    module ParserMonad = struct
+      type 'output t =
+        IS.stream -> (('output * IS.stream), string) result
+      let pure x = fun stream -> PResult.ok (x, stream)
+      let bind prsr k = let open PResult in 
+                        fun input ->
+                        let* (result1, remainder1) = prsr input in
+                        (k result1) remainder1
+    end
+    include Monad2App (ParserMonad)
+    
+    let run_parser prsr input = prsr input
+    
+    module Combinators = struct
+
+      let alternative prsr1 prsr2 input =
+        match prsr1 input with
+        | Error _ -> prsr2 input
+        | _ -> prsr1 input
+      
+      let (<|>) = alternative
+      
+      let succeed input = PResult.ok input
+
+      let fail _ = PResult.error "error: pfail"
+
+      let choice prsrs = List.foldl (<|>) fail prsrs
+
+      let optional prsr = prsr *> pure () <|> pure ()
+      
+      let satisfy pred = function
+        | r when IS.null r -> PResult.error "end of file"
+        | toks -> begin
+            if pred (IS.hd toks)
+            then PResult.ok (IS.hd toks, IS.tl toks)
+            else PResult.error "error: satisfy"
+          end
+      
+      let eof = function
+        | r when r = IS.empty -> PResult.ok ((), IS.empty)
+        | _ -> PResult.error "error: eof"
+
+      let token tok = satisfy (fun x -> x = tok)
+
+      let rec many prsr input =
+        match prsr input with
+        | Ok _ -> (pure OS.cons <*> prsr <*> many prsr) input
+        | Error _ -> (pure OS.empty) input
+
+      let many1 prsr = pure OS.cons <*> prsr <*> many prsr
+
+      let sep_by1 prsr sepPrsr =
+        let+ initial = many (prsr <* sepPrsr)
+        and+ final = prsr
+        in
+        OS.(append initial @@ cons final empty)
+
+      let munch1 pred = many1 (satisfy pred)
+
+      let token tok = satisfy (eq tok)
+
+      let tokens toks = 
+        let concat_tok toksP tok =
+          let+ toks = toksP
+          and+ token = token tok in
+          OS.(append toks @@ cons token empty)
+        in
+        OS.(foldl concat_tok @@ pure empty) toks
+      
+      let chainl op p =
+        let rec apply_all x = function
+          | s when OS.null s -> x
+          | stream -> apply_all ((OS.hd stream) x) (OS.tl stream)
+        in
+        apply_all <$> p <*> many (flip <$> op <*> p)
+      
+      let any_op input =
+        let op (func, toks) = pure func <* tokens toks in
+        (choice << List.map op) input
+
+      let mk_expr atomic ranking left right =
+        let pack l prsr r = tokens l *> prsr <* tokens r in
+        let rec expr input = (foldr chainl factor ranking) input
+        and factor input = (atomic <|> pack left expr right) input
+        in (expr, factor)
+    end
+    include Combinators
   end
-  include Monad2App (ParserMonad)
-       
-  let run_parser prsr input = prsr input
-            
-  module Combinators = struct
-
-    let alternative prsr1 prsr2 input =
-      match prsr1 input with
-      | Error _ -> prsr2 input
-      | _ -> prsr1 input
-    
-    let (<|>) = alternative
-    
-    let succeed input = PResult.ok input
-
-    let fail _ = PResult.error "error: pfail"
-
-    let choice prsrs = List.foldl (<|>) fail prsrs
-
-    let optional prsr = prsr *> pure () <|> pure ()
-                     
-    let satisfy pred = function
-      | r when IS.null r -> PResult.error "end of file"
-      | toks -> begin
-          if pred (IS.hd toks)
-          then PResult.ok (IS.hd toks, IS.tl toks)
-          else PResult.error "error: satisfy"
-        end
-          
-    let eof = function
-      | r when r = IS.empty -> PResult.ok ((), IS.empty)
-      | _ -> PResult.error "error: eof"
-
-    let token tok = satisfy (fun x -> x = tok)
-
-    let rec many prsr input =
-      match prsr input with
-      | Ok _ -> (pure OS.cons <*> prsr <*> many prsr) input
-      | Error _ -> (pure OS.empty) input
-
-    let many1 prsr = pure OS.cons <*> prsr <*> many prsr
-
-    let sep_by1 prsr sepPrsr =
-      let+ initial = many (prsr <* sepPrsr)
-      and+ final = prsr
-      in
-      OS.(append initial @@ cons final empty)
-
-    let munch1 pred = many1 (satisfy pred)
-
-    let token tok = satisfy (eq tok)
-
-    let tokens toks = 
-      let concat_tok toksP tok =
-        let+ toks = toksP
-        and+ token = token tok in
-        OS.(append toks @@ cons token empty)
-      in
-      OS.(foldl concat_tok @@ pure empty) toks
-    
-    let chainl op p =
-      let rec apply_all x = function
-        | s when OS.null s -> x
-        | stream -> apply_all ((OS.hd stream) x) (OS.tl stream)
-      in
-      apply_all <$> p <*> many (flip <$> op <*> p)
-     
-    let any_op input =
-      let op (func, toks) = pure func <* tokens toks in
-      (choice << List.map op) input
-
-    let mk_expr atomic ranking left right =
-      let pack l prsr r = tokens l *> prsr <* tokens r in
-      let rec expr input = (foldr chainl factor ranking) input
-      and factor input = (atomic <|> pack left expr right) input
-      in (expr, factor)
-
-  end
-  include Combinators
 end
 
 
@@ -435,7 +437,7 @@ module Example = struct
             | Seq.Cons _ -> false
       end
 
-      module Lexer = ParserF (SeqTok) (SeqTok)
+      module Lexer = Parser.Make (SeqTok) (SeqTok)
 
       let lexP =
         let open Lexer in
@@ -484,7 +486,7 @@ module Example = struct
             | Seq.Cons _ -> false
       end
 
-      module Lexer = ParserF (SeqTok) (SeqTok)
+      module Lexer = Parser.Make (SeqTok) (SeqTok)
 
       let lexP =
         let open Lexer in
@@ -587,14 +589,14 @@ module Example = struct
       
     end
     
-    module Parser = ParserF (SeqTok) (SeqTok)
+    module P = Parser.Make (SeqTok) (SeqTok)
   
     let skip_spaces =
-      let open Parser in
+      let open P in
       optional (satisfy Lex.is_space)
                   
     let numP =
-      let open Parser in
+      let open P in
       let mk_num = function
         | Lex.Num n -> Num n
         | _ -> assert false
@@ -603,7 +605,7 @@ module Example = struct
       in mk_num lexeme
        
     let rec binopP input =
-      let open Parser in
+      let open P in
       let open Lex in
       let op prsr pred =
           pure prsr
@@ -625,7 +627,7 @@ module Example = struct
         ] @@ input
       
     and expP input =
-      let open Parser in
+      let open P in
       choice [
           pure mk_numexp <*> numP ;
           pure mk_opexp <*> binopP ;
