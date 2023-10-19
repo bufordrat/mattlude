423df66345552c9ed48b1fb74ad257c051fbc284
Matt Teichman <teichman@uchicago.edu>
2023-10-19 14:35:18 -0500

    break option out into separate .ml file

---
+++ src/option.ml
@@ -0,0 +1,20 @@
+include Prelude.Option
+include Stdlib.Option
+
+module Monad = Endofunctors.Monad 
+
+let cat_options lst =
+  let rec cat_options' acc = function
+    | [] -> acc
+    | Some x :: xs -> cat_options' (x :: acc) xs
+    | None :: xs -> cat_options' acc xs
+  in
+  List.rev @@ cat_options' [] lst
+
+module OptionMonad = struct
+  type 'a t = 'a option
+  let pure = some
+  let bind = bind
+end
+
+include Monad.Make (OptionMonad)
