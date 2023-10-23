8d12ac9acd6ae0687725720128f9fe268543d031
Matt Teichman <teichman@uchicago.edu>
2023-10-23 17:18:05 -0500

    rearrange code in option module

--- src/option.ml
+++ src/option.ml
@@ -3,14 +3,6 @@ include Stdlib.Option
 
 module Monad = Endofunctors.Monad 
 
-let cat_options lst =
-  let rec cat_options' acc = function
-    | [] -> acc
-    | Some x :: xs -> cat_options' (x :: acc) xs
-    | None :: xs -> cat_options' acc xs
-  in
-  List.rev @@ cat_options' [] lst
-
 module OptionMonad = struct
   type 'a t = 'a Stdlib.Option.t
   let pure = some
@@ -18,3 +10,11 @@ module OptionMonad = struct
 end
 
 include Monad.Make (OptionMonad)
+
+let cat_options lst =
+  let rec cat_options' acc = function
+    | [] -> acc
+    | Some x :: xs -> cat_options' (x :: acc) xs
+    | None :: xs -> cat_options' acc xs
+  in
+  List.rev @@ cat_options' [] lst
