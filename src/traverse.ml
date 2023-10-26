62016f1f3e6626c3fd0ed98aab225e4c0d6afedc
Matt Teichman <teichman@uchicago.edu>
2023-10-26 16:25:19 -0500

    preliminary list traverse

--- src/traverse.ml
+++ src/traverse.ml
@@ -9,5 +9,19 @@ module Make (S : STREAM) (I : IDIOM) = struct
 
   module Idiom = Applicative.Make (I)
   module Stream = Functor.Make (S)
+end
 
+module List = struct
+  module Make (I : IDIOM) = struct
+    open Endofunctors
+    include Applicative.Make (I)
+    let sequence lst =
+      let reducer acc ax =
+        let+ x = ax
+        and+ xs = acc
+        in x :: xs
+      in
+      let open Prelude in
+      foldl reducer (I.pure []) lst >>| rev
+  end
 end
